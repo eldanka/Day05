9d9244d6048725d73f0552cec3695d86ba0c2c81
eldanka <eldankahrovic2208@gmail.com>
2021-03-12 19:43:12 +0100

    Adding ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+
+    var myName = "Eldan";
+    var myString = "My name is " + myName + ", how are you?";
+
+    return myString;
+
+}
+
+console.log(main());
+module.exports = main;
